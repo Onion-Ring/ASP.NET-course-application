7dc809d22e75788437494375a41ca41c115ecb51
OnionRing <antoniocg285@gmail.com>
2025-03-31 15:14:39 +0200

    Initial-commit
    Project created and gitignore file added

---
+++ V6_ASP.NET_Course/V6_ASP.NET_Course/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace V6_ASP.NET_Course.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
